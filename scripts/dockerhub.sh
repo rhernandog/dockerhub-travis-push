26fc8688e08c9ffb1c5fe746f2ebfc98714a4f58
Rodrigo Hernando <rhernandog@gmail.com>
2020-05-18 11:17:10 -0400

    changes in conditional logic on shell file

--- scripts/dockerhub.sh
+++ scripts/dockerhub.sh
@@ -9,14 +9,14 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
   exit 0
 fi
 
-if [["$TRAVIS_PULL_REQUEST" == true && "$TRAVIS_PULL_REQUEST_BRANCH" != "master"]]; then
+if [[ "$TRAVIS_PULL_REQUEST" == true && "$TRAVIS_PULL_REQUEST_BRANCH" != "master" ]]; then
   echo "this is a pull request"
   echo "$TRAVIS_PULL_REQUEST"
   echo "$TRAVIS_PULL_REQUEST_BRANCH"
   exit 0
 fi
 
-if [[ ! "$TRAVIS_PULL_REQUEST" && "$TRAVIS_BRANCH" == "master"]]; then
+if [[ "$TRAVIS_PULL_REQUEST" == false && "$TRAVIS_BRANCH" == "master"]]; then
   echo "We are in the master branch"
   echo "This is not a PR, most likely a merge"
   exit 0
