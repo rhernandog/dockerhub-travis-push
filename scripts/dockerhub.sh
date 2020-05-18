ca991066be293bc815161bdcf6a3e292fba42a9d
Rodrigo Hernando <rhernandog@gmail.com>
2020-05-18 00:02:24 -0400

    Another bash try

--- scripts/dockerhub.sh
+++ scripts/dockerhub.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-  echo "We're not on the master branch."
+  echo "We're not on the master branch!!."
   # This will analyze the current branch TheDFury and react accordingly
   exit 0
 fi
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  echo "We ARE on the master branch."
+  echo "We ARE on the master branch!!!."
   # This will analyze the current branch TheDFury and react accordingly
   exit 0
 fi
