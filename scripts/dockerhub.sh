34330b4463250db9e1403ab79a55c3fb78a6e70b
Rodrigo Hernando <rhernandog@gmail.com>
2020-05-18 00:26:03 -0400

    ....

--- scripts/dockerhub.sh
+++ scripts/dockerhub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[$TRAVIS_BRANCH != "master"]]; then
+if [["$TRAVIS_BRANCH" != "master"]]; then
   echo "We're not on the master branch!!."
   # This will analyze the current branch and react accordingly
   exit 0
