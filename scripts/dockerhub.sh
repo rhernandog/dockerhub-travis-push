854746fef9af83b79c3d0b8ca009aef1063d32af
Rodrigo Hernando <rhernandog@gmail.com>
2020-05-18 12:00:42 -0400

    Docker Hub Push Final Update - Removed commented out code from shell file - Updated Readme file

--- scripts/dockerhub.sh
+++ scripts/dockerhub.sh
@@ -17,14 +17,3 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
   docker push rhernandog/dockerhub-push-test
   exit 0
 fi
-
-# if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-#   echo "We ARE on the master branch!!!."
-#   echo "$TRAVIS_BRANCH"
-#   echo "$TRAVIS_PULL_REQUEST"
-#   echo "$TRAVIS_PULL_REQUEST_BRANCH"
-#   # docker build -t rhernandog/multisample-one-worker ./worker
-#   # echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
-#   # docker push rhernandog/multisample-one-client
-#   exit 0
-# fi
