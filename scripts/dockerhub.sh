f1902804237742181544740c38edcc4a762b9404
Rodrigo Hernando <rhernandog@gmail.com>
2020-05-18 10:43:31 -0400

    Added pull request and pull request branch on shell file

--- scripts/dockerhub.sh
+++ scripts/dockerhub.sh
@@ -3,6 +3,8 @@
 if [[ "$TRAVIS_BRANCH" != "master" ]]; then
   echo "We're not on the master branch!!."
   echo "$TRAVIS_BRANCH"
+  echo "$TRAVIS_PULL_REQUEST"
+  echo "$TRAVIS_PULL_REQUEST_BRANCH"
   # This will analyze the current branch TheDFury and react accordingly
   exit 0
 fi
@@ -10,6 +12,8 @@ fi
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   echo "We ARE on the master branch!!!."
   echo "$TRAVIS_BRANCH"
+  echo "$TRAVIS_PULL_REQUEST"
+  echo "$TRAVIS_PULL_REQUEST_BRANCH"
   # docker build -t rhernandog/multisample-one-worker ./worker
   # echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
   # docker push rhernandog/multisample-one-client
